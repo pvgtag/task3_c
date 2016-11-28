c1756fc938215b71da376994bf4d07174b163914
Vadim Petrov <pvg.tag@yandex.ru>
2016-11-28 16:05:50 +0300

    Add 'fat' metrics

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Promise from 'bluebird';
 import _ from 'lodash';
 // import canonize from './canonize';
 
-// const __DEV__ = true;
+const __DEV__ = true;
 
 const app = express();
 app.use(cors());
@@ -19,58 +19,116 @@ app.use(cors());
 // });
 
 const baseUrl = 'https://pokeapi.co/api/v2';
-const pokemonFields = ['id', 'name', 'weight'];
+const pokemonFields = ['id', 'name', 'weight', 'height'];
+const pokemonFieldsFinish = ['name'];
+
 // pokemon
 // pokemon/1
 
-// async function getPokemons(url, i = 0) {
-//   console.log('getPokemons', url, i);
-//   const responce = await fetch(url);
-//   const page = await responce.json();
-//   const pokemons = page.results;
-//   if (__DEV__ && i > 1) {
-//     return pokemons;
-//   }
-//   if (page.next) {
-//     const pokemon2 = await getPokemons(page.next, i + 1);
-//     return [
-//       ...pokemons,
-//       ...pokemon2,
-//     ];
-//   }
-//   return pokemons;
-// }
+async function getPokemons(url, i = 0) {
+  console.log('getPokemons', url, i);
+  const responce = await fetch(url);
+  const page = await responce.json();
+  const pokemons = page.results;
+  if (__DEV__ && i > 1) {
+    return pokemons;
+  }
+  if (page.next) {
+    const pokemon2 = await getPokemons(page.next, i + 1);
+    return [
+      ...pokemons,
+      ...pokemon2,
+    ];
+  }
+  return pokemons;
+}
 
-// async function getPokemon(url) {
-//   const responce = await fetch(url);
-//   const pokemon = await responce.json();
-//   return pokemon;
-// }
+async function getPokemon(url) {
+  console.log(url);
+  const responce = await fetch(url);
+  const pokemon = await responce.json();
+  return pokemon;
+}
 
 // //const pokemonsUrl = `${baseUrl}/pokemon/`;
 // //getPokemons(pokemonsUrl).then((pokemons) => {
 // //  console.log(pokemons.length);
 // //});
 
-app.get('/', async (req, res) => {
-  // try {
-  //   const pokemonsUrl = `${baseUrl}/pokemon/`;
-  //   const pokemonsInfo = await getPokemons(pokemonsUrl);
-  //   const pokemonPromises = pokemonsInfo.map((info) => {
-  //     return getPokemon(info.url);
-  //   });
-  //   const pokemonsFull = await Promise.all(pokemonPromises);
-  //   const pokemons = pokemonsFull.map((pokemon) => {
-  //     return _.pick(pokemon, pokemonFields);
-  //   });
+// app.get('/angular', async (req, res) => {
+//   console.log(req.query);
+//   return res.send('OK');
+// });
 
-  //   const sortPokemons = _.sortBy(pokemons, pokemon => pokemon.weight);
-  //   return res.json(sortPokemons);
-  // } catch (error) {
-  //   console.log(error);
-  //   return res.json({ error });
-  // }
-  return res('OK');
+// app.get('/huge', async (req, res) => {
+//   console.log(req.query);
+//   return res.send('OK');
+// });
+
+// app.get('/micro', async (req, res) => {
+//   console.log(req.query);
+//   return res.send('OK');
+// });
+
+app.get('/fat', async (req, res) => {
+  const limit = req.query.limit ? +req.query.limit : 20;
+  const offset = req.query.offset ? +req.query.offset : 0;
+  try {
+    const pokemonsUrl = `${baseUrl}/pokemon`;
+    const pokemonsInfo = await getPokemons(pokemonsUrl);
+    const pokemonPromises = pokemonsInfo.map((info) => {
+      return getPokemon(info.url);
+    });
+    const pokemonsFull = await Promise.all(pokemonPromises);
+
+    const pokemons = pokemonsFull.map((pokemon) => {
+      return _.pick(pokemon, pokemonFields);
+    });
+    // const sortPokemons = _.sortBy(pokemons, pokemon => pokemon.name);
+    const sortPokemons = _.orderBy(pokemons, ['weight / height', 'name']);
+    const slPokemons = sortPokemons.slice(offset, offset + limit);
+    const resPokemons = slPokemons.map((pokemon) => {
+      return _.pick(pokemon, pokemonFieldsFinish);
+    });
+    console.log('!!!!');
+    return res.json(resPokemons);
+  } catch (error) {
+    console.log(error);
+    return res.json({ error });
+  }
+});
+
+// app.get('/heavy', async (req, res) => {
+//   console.log(req.query);
+//   return res.send('OK');
+// });
+
+app.get('/', async (req, res) => {
+  const limit = req.query.limit ? +req.query.limit : 20;
+  const offset = req.query.offset ? +req.query.offset : 0;
+  try {
+    const pokemonsUrl = `${baseUrl}/pokemon`;
+    const pokemonsInfo = await getPokemons(pokemonsUrl);
+    const pokemonPromises = pokemonsInfo.map((info) => {
+      return getPokemon(info.url);
+    });
+    const pokemonsFull = await Promise.all(pokemonPromises);
+
+    const pokemons = pokemonsFull.map((pokemon) => {
+      return _.pick(pokemon, pokemonFields);
+    });
+    const sortPokemons = _.sortBy(pokemons, pokemon => pokemon.name);
+    const slPokemons = sortPokemons.slice(offset, offset + limit);
+    const resPokemons = slPokemons.map((pokemon) => {
+      return _.pick(pokemon, pokemonFieldsFinish);
+    });
+    console.log('!!!!');
+    return res.json(resPokemons);
+  } catch (error) {
+    console.log(error);
+    return res.json({ error });
+  }
+  // return res.send('OK');
 });
 
 app.listen(3000, () => {
